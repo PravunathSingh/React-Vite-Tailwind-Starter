2ca64721750cd3b811c53757a9cf16906c6f8277
Pravunath Singh <pravunathsingh@gmail.com>
2022-04-09 11:19:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='h-screen container bg-cyan-800 flex justify-center items-center'>
+      <div className='rounded-xl shadow-lg p-6 max-w-2xl mx-auto bg-slate-100'>
+        <h1 className='text-lg md:text-xl lg:text-2xl font-medium leading-relaxed md:leading-relaxed lg:leading-relaxed'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
+          officiis, amet perferendis voluptatem aut nam accusantium similique ut
+          blanditiis quisquam minus repellat quia eius, beatae doloribus saepe?
+          Eaque, ea molestiae.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        sm: '2rem',
+        lg: '3rem',
+        xl: '4rem',
+        '2xl': '5.5rem',
+        '3xl': '6.5rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
